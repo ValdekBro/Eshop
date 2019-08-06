975ba58b0a854c3b7550d8aadcadde0359018ad7
Yaroslav Badyora <profittom5@gmail.com>
2019-08-06 04:05:51 +0300

    Make  products catalog

--- app/Controllers/Http/CategoryController.js
+++ app/Controllers/Http/CategoryController.js
@@ -1,9 +1,19 @@
 'use strict'
 
-class CategoryController {
-    async index({ request, view, response }) {
+const Category = use('App/Models/Category');
 
-        return view.render('in_developing');
+class CategoryController {
+    async index({ request, view, session, response }) {
+        const categories = await Category.all()
+        .catch( function(e) {
+            session.put('error', e.toString());
+            session.put('error_code', e.code);
+            session.put('error_message', e.sqlMessage);
+            session.put('error_beauti_message', "Failed to load Categories data");
+            console.error(e);
+            response.redirect('/');
+        });
+    return view.render('product.catalog', { categories : categories.toJSON() });
     }
 }
 
--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -1,9 +1,40 @@
 'use strict'
 
+const Category = use('App/Models/Category');
+
+
 class ProductController {
     async index({ request, view, response }) {
+        const category_id  = request.input('category', '1');
+        const category = await Category
+            .find(category_id)
+            .catch( function(e) {
+                session.put('error', e.toString());
+                session.put('error_code', e.code);
+                session.put('error_message', e.sqlMessage);
+                session.put('error_beauti_message', "Failed to find Category data");
+                console.error(e);
+                response.redirect('/catalog');
+            });
+        if(!category) return response.status(404).send('Failed to find Category data')   
 
-        return view.render('in_developing');
+        const products = await category
+            .products()
+            .fetch()
+            .catch( function(e) {
+                session.put('error', e.toString());
+                session.put('error_code', e.code);
+                session.put('error_message', e.sqlMessage);
+                session.put('error_beauti_message', "Failed to load Products data");
+                console.error(e);
+                response.redirect('/catalog');
+            }); 
+            
+        // return view.render('product.catalog', { categories : categories.toJSON() });
+        return view.render('product.list',  { 
+            category : category.toJSON(),
+            products : products.toJSON()
+        });
     }
     async show({ request, view, response }) {
 
